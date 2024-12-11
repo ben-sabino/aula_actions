db0ccb2239245058af88ad57430e28a277cce08a
Benjamim <benjamim.sabino@ufpi.edu.br>
2024-12-10 23:30:07 -0300

    fixes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Acesse meu Github AQUI
+          Acesse meu Github
         </a>
       </header>
     </div>
